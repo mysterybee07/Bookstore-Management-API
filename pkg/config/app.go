58206ad707381b3fc2e2df1144b134e8041bb81c
mysterybee07 <biraj2019pudasaini@gmail.com>
2023-10-28 11:15:51 +0545

    Book store management system fully added

---
+++ pkg/config/app.go
@@ -0,0 +1,30 @@
+package config
+
+import (
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+)
+
+var db *gorm.DB
+
+//	func Connect() {
+//		d, err := gorm.Open("mysql", "Onlycab:Biraj123*/bookstore?charset=utf8&parseTime=True&loc=Local")
+//		if err != nil {
+//			panic(err.Error())
+//		}
+//		db = d
+//	}
+func Connect() {
+	// Create a properly formatted connection string
+	connectionString := "Onlycab:Biraj123*@tcp(localhost:3306)/bookstore?charset=utf8&parseTime=True&loc=Local"
+
+	d, err := gorm.Open("mysql", connectionString)
+	if err != nil {
+		panic(err.Error())
+	}
+	db = d
+}
+
+func GetDB() *gorm.DB {
+	return db
+}
